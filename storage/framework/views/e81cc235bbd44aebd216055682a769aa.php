a0d60e80471575ce5a5a5f50a7d6c462770b2b38
David <142915031+vuthDavid@users.noreply.github.com>
2025-01-13 14:08:12 +0700

    Add files via upload

---
+++ storage/framework/views/e81cc235bbd44aebd216055682a769aa.php
@@ -0,0 +1,3 @@
+<h1>Contact from <?php echo e($name); ?></h1>
+<h3><?php echo e($body); ?></h3>
+From the <?php echo e($email); ?><?php /**PATH C:\laragon\www\portfolio\resources\views/emails/contant.blade.php ENDPATH**/ ?>
