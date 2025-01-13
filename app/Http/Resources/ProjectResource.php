435e79db0ebe55fbd318fc1d5abe6a0a8330e9e3
David <142915031+vuthDavid@users.noreply.github.com>
2025-01-13 14:02:20 +0700

    Add files via upload

---
+++ app/Http/Resources/ProjectResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        // return parent::toArray($request);
+        return[
+            'id' =>$this->id,
+            'name' =>$this->name,
+            'image'=>asset('storage/'. $this->image),
+            'skill' => new SkillResource($this->whenLoaded('skill')),
+            'project_url' =>$this->project_url,
+
+        ];
+    }
+}
