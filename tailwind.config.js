7988b9ddbb8ff35e0f954749f66135af9640b085
David <142915031+vuthDavid@users.noreply.github.com>
2025-01-13 14:11:22 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,52 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.vue',
+    ],
+
+    theme: {      
+            fontFamily: {
+                // sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                primary:"Payfair Display",
+                body: "work Sans"
+            },
+            container:{
+                padding:{
+                    default:"1rem",
+                    lg:"3rem",
+                },
+            },
+            extend:{
+                colors:{
+                    "light-primary": "#FAF1E6",
+                    "light-secondary":"#FAF1F6",
+                    "light-tail-100" : "#A9DED2",
+                    "light-tail-500" : "#54BAB9",
+                    "light-primary" : "#050402",
+                    "dark-secondary" : "#1C1D24",
+                    "dark-navy-100" : "#07567D",
+                    "dark-navy-500" : "#292D42",
+                accent:{
+                    default:"#ac6b34",
+                    hover:"#925a2b",
+                    },
+                    paragraph:"#878e99",
+                    blue:colors.blue,
+                    indigo:colors.indigo,
+                    green:colors.green,
+                    red:colors.red
+
+                },
+            },
+        },
+  
+
+    plugins: [forms],
+};
